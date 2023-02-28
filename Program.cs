7da83305593e724356084de5816ba0b8aebbd58f
tkylesh <taylorshuff90@gmail.com>
2023-02-28 10:08:43 -0600

    phase 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace c61_guessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Display a message to the user asking them to guess the secret number.
+            Console.WriteLine("Guess the secret number: ");
+
+            //Display a prompt for the user's guess.
+            //Take the user's guess as input and save it as a variable.
+            string answer = Console.ReadLine();
+                       
+
+            //Display the user's guess back to the screen.
+            Console.WriteLine("You guessed: " + answer);
         }
     }
 }
