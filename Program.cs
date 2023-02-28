440cae6525ffd5bf905cfe5268a2f37f8bb5bb84
tkylesh <taylorshuff90@gmail.com>
2023-02-28 10:27:48 -0600

    phase4

--- Program.cs
+++ Program.cs
@@ -12,15 +12,20 @@ namespace c61_guessingGame
 
             int secretNumber = 42;
             //Give the user four chances to guess the number.
-            for (int i = 0; i < 4; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                Console.WriteLine("Guess the secret number: ");
+                //Display the number of the user's current guess in the prompt. 
+                //For example, if the user has already guessed one time, the prommpt 
+                //should say something like Your guess (2)>.
+                Console.WriteLine($"Guess the secret number (Guess {i}): ");
                 int answer = int.Parse(Console.ReadLine());
 
                 //Continue to display the success or failure messages as in phase 2
                 if (answer == secretNumber)
                 {
                     Console.WriteLine("You guessed the secret number!");
+                    // End the loop early if the user guesses the correct number.
+                    break;
                 }
                 else
                 {
