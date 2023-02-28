dab1db5aebe6e0fd9daeb0cfcc597a3d1dfe7756
tkylesh <taylorshuff90@gmail.com>
2023-02-28 10:16:34 -0600

    phase2

--- Program.cs
+++ Program.cs
@@ -6,16 +6,27 @@ namespace c61_guessingGame
     {
         static void Main(string[] args)
         {
-            //Display a message to the user asking them to guess the secret number.
             Console.WriteLine("Guess the secret number: ");
 
-            //Display a prompt for the user's guess.
-            //Take the user's guess as input and save it as a variable.
             string answer = Console.ReadLine();
-                       
+                    
 
-            //Display the user's guess back to the screen.
-            Console.WriteLine("You guessed: " + answer);
+            /*Create a variable to contain the secret number. 
+            This number should be hard-coded for now. 42 is a nice number.*/
+            int secretNumber = 42;
+
+            //No longer display the user's guess. They know what they guessed, right?
+
+            /*Compare the user's guess with the secret number. Display
+            a success message if the guess is correct, otherwise display a failure message.*/
+            if(int.Parse(answer) == secretNumber)
+            {
+                Console.WriteLine("You guessed the secret number!");
+            }
+            else
+            {
+                Console.WriteLine("You did not guess the secret number.");
+            }
         }
     }
 }
